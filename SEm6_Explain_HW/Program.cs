69220aecd3daf5f5e584ef74dd7a53f5eeb01681
Expertz <xeniapliner@gmail.com>
2023-07-17 18:40:06 +0300

    Max Min Task with methods

---
+++ SEm6_Explain_HW/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿/*
+Задача 38: Задайте массив случайных вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+//Create array 
+double[] GetRandomDoubleArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0 ; i < array.Length; i++)
+       {
+        array[i] = Math.Round(new Random().NextDouble() * new Random().Next(1, 11), 2); 
+        }
+    return array;
+}
+
+//Print array. How to print array through the string
+
+void PrintDoubleArrayConsole(double[] array)
+{
+string str = String.Join("  ", array);
+Console.Write($"Array:   {str}");
+
+}
+
+double[] array = GetRandomDoubleArray(10);
+PrintDoubleArrayConsole(array);
+
+double max = array.Max();
+double min = array.Min();
+/*for (int i = 0 ; i < array.Length; i++)
+    {
+        if(array[i] < min)
+       min = array[i];
+        if(array[i] > max)
+        max = array[i];
+
+    }
+*/
+Console.WriteLine($"The difference between array MAX {max} and MIN {min} is {Math.Round(max - min, 2)}");  
